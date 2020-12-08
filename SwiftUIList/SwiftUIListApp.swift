7101f55ac4958ebd3cc964b41eadbea9739adb1f
Sullivan677 <decarli.sulli@gmail.com>
2020-12-08 20:34:53 +0100

    Initial Commit

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Sullivan De carli on 08/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
